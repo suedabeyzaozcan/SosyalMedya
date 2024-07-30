e87d1d67918a5af562747cc37bf6844dd553ee79
Sueda Beyza Özcan <104599567+suedabeyzaozcan@users.noreply.github.com>
2024-07-30 17:28:50 +0300

    Add files via upload

---
+++ sosyalmedya/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  sosyalmedya
+//
+//  Created by Sueda Beyza Özcan on 30.07.2024.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
